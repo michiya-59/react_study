4ccf94972c12092c935461f16dbe04ef4a3d1af1
michiya-59 <nishino.michiya0509@gmail.com>
2025-02-09 22:32:10 +0900

    ポケモン図鑑の作成

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { getAllPokemon, getPokemon } from './utils/pokemon';
+import Card from './components/Card';
+import NavBar from './components/NavBar';
+
+function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+
+      // 各ポケモンの詳細データを取得
+      leadPokemon(res.results);
+
+      setNextUrl(res.next);
+      setPrevUrl(res.previous);
+
+      setLoading(false)
+    };
+    fetchPokemonData();
+  },[]);
+
+  const leadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData)
+  }
+
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl)
+    await leadPokemon(data.results)
+
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+
+    setLoading(false);
+  }
+
+  const handlePrevPage = async () => {
+    if(!prevUrl) return;
+
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+
+    await leadPokemon(data.results)
+
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+
+    setLoading(false);
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div className="App">
+        {loading ? (
+          <><h1>ロード中....</h1></>
+        ) : (
+          <>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+
+            <div className="btn">
+              { prevUrl && <button className="button" onClick={() => handlePrevPage()}>前へ</button>}
+              <button className="button" onClick={() => handleNextPage()}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <nav>
+      ポケモン図鑑
+    </nav>
+  )
+}
+
+export default NavBar
